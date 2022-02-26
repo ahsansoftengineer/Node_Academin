489f0add09f4ac93536470a5cb8b6a56bc0d3723
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-02-26 22:59:08 +0500

    EXPRESS | IMPLEMENTATION OVER NODE

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ### Commands
 1. Installing Express
 ##### npm i --save express
+2. Middleware are like funnel like the current middleware will call the next otherwise it will not go further
  
--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const http = require('http');
 // Third Party Modules
 const express = require('express');
 
-app = express()
-
+app = express();
+app.use((req, res, next)=>{
+  console.log('in middleware');
+  next();
+})
+app.use((req, res, next)=>{ 
+  
+  console.log('another middleware'); 
+  // res.send({"name": "Ahsan"});
+  res.send('<h1>Hello from Express </h1>');
+})
 // app doesn't do anything for now
-const server = http.createServer(app)
-
-server.listen(3000);
+app.listen(3000) // Alternative to below Code
+// const server = http.createServer(app)
+// server.listen(3000);
