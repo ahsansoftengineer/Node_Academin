a584e0db292f03bf3ac2a416e87b36fe11688208
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-02-27 10:15:33 +0500

    BODY PARSER | CONVERTING DATA INTO JSON

--- app.js
+++ app.js
@@ -3,19 +3,39 @@ const http = require('http');
 
 // Third Party Modules
 const express = require('express');
+const bodyParser = require('body-parser')
 
 app = express();
-app.use((req, res, next)=>{
-  console.log('in middleware');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', (req, res, next) => {
+  console.log('/', 'This always runs!');
+  // res.redirect('/add-product');
+  next();
+});
+
+app.get('/add-product', (req, res, next) => {
+  console.log('/add-product');
+  res.send(`
+    <form action='/product' method='post'>
+      <input type='text' name='title' />
+      <button type='submit'>Submit</button>
+    </form>
+  `)
+});
+
+app.post('/product', (req, res, next) => {
+  console.log('/product');
+  console.log(req.body);
+  res.redirect('/');
+});
+app.use('/', (req, res, next) => {
+  console.log('/', 'Last Middleware');
+  // Some Process
   next();
 })
 app.use((req, res, next)=>{ 
-  
-  console.log('another middleware'); 
   // res.send({"name": "Ahsan"});
-  res.send('<h1>Hello from Express </h1>');
+  res.send('<h1>Last Middleware </h1>');
 })
-// app doesn't do anything for now
-app.listen(3000) // Alternative to below Code
-// const server = http.createServer(app)
-// server.listen(3000);
+app.listen(3000)
