7c67297864c8f7eda601f3ab5404869ad135e8ff
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-02-28 09:32:28 +0500

    TEMPLETING ENGINES | EJS, PUG, HANDLEBARS

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const http = require('http');
-const routes = require('./routes')
+const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler)
+const app = express();
 
-server.listen(3000);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
