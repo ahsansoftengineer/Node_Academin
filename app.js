ba7f6efd652e63ceb922a64eeec0963dc1554f78
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-02-28 10:55:27 +0500

    TEMPLATE ENGINE | HANDLE BAR IS WORKING

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
-// Default for Pug
-app.set('views', 'views'); 
+// Templating Engine HandleBar is not Working
+var exphbs  = require('express-handlebars');
+
+var hbs = exphbs.create({ extname: '.hbs' /* config */ });
+
+// Register `hbs.engine` with the Express app.
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+// Templating Engine PUG
+// app.set('view engine', 'pug');
+// app.set('views', 'views'); // Default for Pug
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
