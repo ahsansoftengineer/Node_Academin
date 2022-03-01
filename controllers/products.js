586b719796abfd0f28760793c9068a90b3df4790
DAWATEISLAMI\KFMMA1597 <it.dev57@dawateislami.net>
2022-03-01 14:52:54 +0500

    TRANSACTION | DEPOSIT -> bank_id into account_id

---
+++ controllers/products.js
@@ -0,0 +1,9 @@
+exports.addProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,16 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const productAdd = require('../controllers/products')
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', );
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
