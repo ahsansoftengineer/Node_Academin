f3b3fb671b70804c1ce531be3dc721a63c7847a9
DAWATEISLAMI\KFMMA1597 <it.dev57@dawateislami.net>
2022-03-01 17:06:49 +0500

    MVC | 404 NOT Working

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,24 @@
-exports.addProduct = (req, res, next) => {
+const productz = []
+exports.addGet = (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
+  }); 
+}
+exports.addPost = (req, res, next) => {
+  productz.push({ title: req.body.title });
+  res.redirect('/');
+}
+exports.listGet = (req, res, next) => {
+  res.render('shop', {
+    prods: productz,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: productz.length > 0,
+    activeShop: true,
+    productCSS: true
   });
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,14 @@
-// const path = require('path');
-
 const express = require('express');
-
-// const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const productAdd = require('../controllers/products')
+const product = require('../controllers/products')
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', );
+router.get('/add-product', product.addGet);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
+router.post('/add-product', product.addPost);
 exports.routes = router;
 exports.products = products;
