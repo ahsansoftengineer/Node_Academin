337a3ca513bc1d1ddf53df8a95bc0a4396ac0af6
DAWATEISLAMI\KFMMA1597 <it.dev57@dawateislami.net>
2022-03-01 14:06:16 +0500

    Format | Shifted

--- app.js
+++ app.js
@@ -4,21 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 // 1. Templating Engine EJS
 app.set('view engine', 'ejs');
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-
 app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
-
 app.listen(3000);
